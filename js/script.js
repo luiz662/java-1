21ae5cb3be3428e12e538f8383f757d463ca3343
luiz662 <141882626+luiz662@users.noreply.github.com>
2024-10-29 21:24:59 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,41 @@
+/* alert(" vo");
+
+prompt("nn"); */
+
+/* var vagaEstacionamento = "carro branco"
+console.log(vagaEstacionamento)
+
+var  vagaEstacionamento = "carro vermelho"
+
+console.log(vagaEstacionamento) */
+
+
+
+
+
+/* var nome = prompt("digite seu nome:")
+alert(" Olá " + nome + " seja bem vindo ")
+
+alert(` Olá ${nome}, seja bem vindo!`) */
+/* 
+var primeiroValor =  Number(prompt(" digite um numero: ")) 
+var segundoValor = Number(prompt(" digite outro valor:")) 
+
+var resultadoSoma = primeiroValor + segundoValor
+console.log(primeiroValor)
+console.log(segundoValor)
+alert( ` o resultado da soma = ${resultadoSoma}`)
+ */
+
+
+    var nome = prompt("digite seu nome ")
+    var idade =  prompt("digite sua idade")
+    var cidade =  prompt("digite sua cidade")
+    var profissao =  prompt("digite sua profissao")
+
+    alert (` seu nome: ${nome}
+        sua idade: ${idade}
+        sua cidade: ${cidade}
+        sua profissao: ${profissao}
+    
+        `)
